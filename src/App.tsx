94436cb83fa79a515ac94c34e1167e7a19c2d57c
gemario <gmario.sh@gmail.com>
2022-10-14 05:47:44 -0300

    After

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { SignIn } from './pages/Signin';
+
 import './styles/global.css';
 
 export function App() {
-
   return (
-   <h1 className='font-bold text-5xl text-violet-500'>Hello world</h1>
+    <SignIn/>
   )
 }
 
